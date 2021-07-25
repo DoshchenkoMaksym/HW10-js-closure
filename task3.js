fe651746ef723aae1cd511697bd643de4c976433
Maksym Doshchenko <doschenko54@gmail.com>
2021-07-25 17:10:13 +0300

    finished task3

---
+++ task3.js
@@ -0,0 +1,15 @@
+function sum(x) {
+    let calc = x;
+    return function(y) {
+        return calc = x + y;
+    };
+};
+
+const sum1 = sum(5);
+sum1(2);
+
+ /*    const sum1 = sum(1);
+    sum1(2); // результат выполнения 3
+    или 
+        вызов sum(1)(2); // результат выполнения 3
+     */
